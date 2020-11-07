ed130c3cb9db7dc0951da12c6e31ecbc369561e0
Gian Laager <gianlaager114@gmail.com>
2020-11-07 18:03:56 +0100

    new PIDConfigurator class

---
+++ src/main/java/frc/robot/PIDConfig.java
@@ -0,0 +1,122 @@
+package frc.robot;
+
+import java.util.LinkedHashMap;
+import java.util.Optional;
+
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class PIDConfig {
+	/**
+	 * <h> PIDConfigurator class wich configures the PID off a WPI_TalonSRX </h>
+	 * 
+	 * @see
+	 * <p>
+	 * 	Please use the Double-Brace initialization, like in the example above the class definition, to keep
+	 * 	it straightforward.
+	 * </p>
+	 * @see
+	 * <p>
+	 * 	You have to set all non optional values or the defualt will be used. If
+	 * 	you don't initialize the optional values, the associated WPI_TalonSRX
+	 * 	function won't be called
+	 * </p>
+	 */
+
+	/* Example:
+
+	* WPI_TalonSRX myTalonSRX = new WPI_TalonSRX(0);
+	* PIDConfig.PIDConfigurator myPIDConfigurator= new PIDConfig.PIDConfigurator() {{
+	* 	statusFramePeriods.put(StatusFrameEnhanced.Status_10_MotionMagic, 10); // use "statusFramePeriods.put(<StatusFrameEnhanced>, <value>)" to set the statusFramePeriod <StatusFrameEnhanced>
+	* 	kP = 1.0;
+	* 	kI = 2.0;
+	* 	kD = 3.0;
+	* 	
+	* // ...
+	* 	
+	* 	allowableClosedloopError = Optional.of(0); // use "= Optional.of(<yourVlaue>)" to set allowableClosedloopError and other all other optional parameters.
+	* }};
+	* myPIDConfigurator.configMotor(myTalonSRX);
+	*
+	*/
+
+	public static class PIDConfigurator {
+		public LinkedHashMap<StatusFrameEnhanced, Integer> statusFramePeriods;
+
+		public double nominalOutputForward = 0.0;
+		public double nominalOutputReverse = 0.0;
+		public double peakOutputForward = 0.0;
+		public double peakOutputReverse = 0.0;
+
+		public int slotIdx = 0;
+		public int pidIdx = 0;
+
+		public double kP = 0.0;
+		public double kI = 0.0;
+		public double kD = 0.0;
+
+		public int integralZone = 0;
+
+		public int cruiseVelocity = 0;
+		public int acceleration = 0;
+
+		public Optional<Integer> allowableClosedloopError;
+		public Optional<Double> openloopRamp;
+		public Optional<Double> closedloopRamp;
+
+		/**
+		 * Configures the given motor with the specified settings from this class.
+		 * @param motor
+		 * The motor to be configured.
+		 */
+		public void configMotor(WPI_TalonSRX motor) {
+			configLoopRamps(motor);
+			setStatusFramePeriods(motor);
+			configOutputs(motor);
+			configPIDConstants(motor);
+			configSpeeds(motor);
+		}
+
+		private void configSpeeds(WPI_TalonSRX motor) {
+			motor.configMotionCruiseVelocity(cruiseVelocity, Constants.timeOut);
+            motor.configMotionAcceleration(acceleration, Constants.timeOut);
+		}
+
+		private void configPIDConstants(WPI_TalonSRX motor) {
+			motor.selectProfileSlot(slotIdx, pidIdx);
+            motor.config_kP(slotIdx, kP, Constants.timeOut);
+            motor.config_kI(slotIdx, kI, Constants.timeOut);
+            motor.config_kD(slotIdx, kD, Constants.timeOut);
+			motor.config_IntegralZone(slotIdx, integralZone);
+			configAllowableClosedloopError(motor);
+		}
+
+		private void configAllowableClosedloopError(WPI_TalonSRX motor) {
+			if (allowableClosedloopError.isPresent())
+            	motor.configAllowableClosedloopError(slotIdx, allowableClosedloopError.get(), Constants.timeOut);
+		}
+
+		private void configOutputs(WPI_TalonSRX motor) {
+			motor.configNominalOutputForward(nominalOutputForward, Constants.timeOut);
+            motor.configNominalOutputReverse(nominalOutputReverse, Constants.timeOut);
+            motor.configPeakOutputForward(peakOutputForward, Constants.timeOut);
+            motor.configPeakOutputReverse(peakOutputReverse, Constants.timeOut);
+		}
+
+		private void setStatusFramePeriods(WPI_TalonSRX motor) {
+			for (StatusFrameEnhanced k : statusFramePeriods.keySet())
+				motor.setStatusFramePeriod(k, statusFramePeriods.get(k));
+		}
+
+		private void configLoopRamps(WPI_TalonSRX motor) {
+			if (openloopRamp.isPresent())
+				motor.configOpenloopRamp(openloopRamp.get());			
+			if (closedloopRamp.isPresent())
+				motor.configOpenloopRamp(closedloopRamp.get());
+		}
+	}
+
+	public static class PIDFConfigurator extends PIDConfigurator {
+		public double kF;
+	}
+}
