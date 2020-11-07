c96749e0419742090a9bdc60a124b63b2472b06c
Gian Laager <gianlaager114@gmail.com>
2020-11-07 17:16:25 +0100

    new setInverted function for ArduinoAbsoluteEncoder

--- src/main/java/frc/ArduinoAbsoluteEncoder/ArduinoAbsoluteEncoder.java
+++ src/main/java/frc/ArduinoAbsoluteEncoder/ArduinoAbsoluteEncoder.java
@@ -15,49 +15,60 @@ public class ArduinoAbsoluteEncoder {
         public static final byte SET_HOME = 0x01;
         public static final byte GET_REL_POSITION = 0x02;
     }
+
+    private boolean inverted = false;
     private I2C device;
+
     /**
-     * Creates an ArduinoEncoder object wich is connected to the arduino trough I2C 
+     * Creates an ArduinoEncoder object wich is connected to the arduino trough I2C
      * 
-     * @param port The I2C port the device is connected to.
+     * @param port          The I2C port the device is connected to.
      * 
      * @param deviceAddress The address of the device on the I2C bus.
      */
-    ArduinoAbsoluteEncoder(I2C.Port port, int deviceAdress) {
+    public ArduinoAbsoluteEncoder(I2C.Port port, int deviceAdress) {
         device = new I2C(port, deviceAdress);
+    }
+
+    public void setInverted(boolean inverted) {
+        this.inverted = inverted;
     }
 
     /**
      * sets The current position of the encoder to 0
      * 
-     * @return
-     * Returns true if transfer was successfull
+     * @return Returns true if transfer was successfull
      */
     public boolean setHome(byte pos) {
         return !device.write(Request.SET_HOME, pos);
     }
 
     /**
-     * @return
-     * Returns the current absolute position of the encoder. 
-     * Returns -1 if the transfer wasn't successfull.
+     * @return Returns the current absolute position of the encoder. Returns -1 if
+     *         the transfer wasn't successfull.
      */
-    public byte getAbsPosition() {
+    public int getAbsPosition() {
         byte[] buffer = new byte[1];
         if (device.read(Request.GET_ABS_POSITION, 1, buffer))
             return -1;
-        return buffer[0];
-    }    
-    
+        if (!inverted)
+            return buffer[0];
+        else
+            return 127 - buffer[0];
+    }
+
     /**
-     * @return
-     * Returns the current relative position of the encoder to the home point. 
-     * Returns -1 if the transfer wasn't successfull.
+     * @return Returns the current relative position of the encoder to the home
+     *         point. Returns -1 if the transfer wasn't successfull.
      */
-    public byte getRelPosition() {
+    public int getRelPosition() {
         byte[] buffer = new byte[1];
         if (device.read(Request.GET_REL_POSITION, 1, buffer))
             return -1;
-        return buffer[0];
+
+        if (!inverted)
+            return buffer[0];
+        else
+            return 127 - buffer[0];
     }
 }
